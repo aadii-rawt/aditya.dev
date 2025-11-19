f5fe31ee37497d3c6ec4beded5ba9c4a0bdfeae3
aadii-rawt <rawatadii060@gmail.com>
2025-11-19 10:54:52 +0530

    overflow fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import QuoteCard from './Components/QuoteCard'
 
 function App() {
   return (
-      <div className='bg-[#151312] text-white w-full min-h-screen'>
+      <div className='bg-[#151312] element text-white w-full min-h-screen'>
         <div className='max-w-[1150px] mx-auto py-24 flex flex-col md:flex-row items-center  md:items-start gap-20 relative '>
           <div className='lg:sticky lg:left-0 lg:top-24 mx-5'>
             <Profile />
